21b39ef2ca13d317cae664ae7f40cff49d5597ba
partnern <partnern@stu.otc.school.nz>
2026-04-22 15:19:33 +1200

    More questions and answers

--- NevePartner 92004.py
+++ NevePartner 92004.py
@@ -7,8 +7,9 @@ print("The first five questions will be multi choice, and the last five will be 
 
 #Creating the first five multi choice questions
 print('For the multi choice questions, please input the number corresponding to the answer you think is correct.')
-answer1 = input("Question one: Which of these places did Lisa Carrington not win an Olympic medal at\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n")
-
+multiquestion1 = input("Question one: Which of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n")
+multianswer1=2
+multiquestion1 = input('What sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoe Slalem\n )
 # #Creating a dictionary with user input questions and answers
 questions_answers = {'What year was Lisa Carrington born?' : 1989,
                      'What sport does Lisa Carrington compete in?' : 'kayaking',
