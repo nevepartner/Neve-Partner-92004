55e8c30ba507e28e8ee579f59c06aa8f09e248d6
partnern <partnern@stu.otc.school.nz>
2026-04-24 09:59:30 +1200

    Finished the questions

--- NevePartner 92004.py
+++ NevePartner 92004.py
@@ -7,13 +7,18 @@ print("The first five questions will be multi choice, and the last five will be 
 
 #Creating the first five multi choice questions
 print('For the multi choice questions, please input the number corresponding to the answer you think is correct.')
-multiquestion1 = input("Question one: Which of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n")
-multianswer1=2
-multiquestion1 = input('What sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoe Slalem\n )
+multiQandA = {"How many Olympic Games has she competed in?\n1. 1\n2. 2\n3. 3\n4. 4\n" : 4,
+                  "Which of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n" : 2,
+                  "What sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoeing\n3. Kayaking\n4. Whitewater rafting\n" : 2,
+                  "When did she obtain the record for world's fastest K1 200m?\n1. 2010\n2.2012\n3. 2014\n4. 2016\n" : 3,
+                  "How many Olympic bronze medals has she won?\n1. 1\n2. 2\n3. 3\n4. 4\n" : 1}
+#Asking the user for the multi choice answers
+for question, answer in multiQandA.items():
+    print(question)
+
 # #Creating a dictionary with user input questions and answers
-questions_answers = {'What year was Lisa Carrington born?' : 1989,
-                     'What sport does Lisa Carrington compete in?' : 'kayaking',
-                     'How many Olympic medals has she won?' : 9,
-                     'What city was she born in?' : 'Tauranga',
-                     'How many World Championship gold medals has she won?' : 15,
-                     'How many Olympic bronze medals has she won?' : 1,}
+questions_answers = {"What year was Lisa Carrington born?" : 1989,
+                     "How many Olympic medals has she won?" : 9,
+                     "What city was she born in?" : "Tauranga",
+                     "How many World Championship gold medals has she won?" : 15,
+                     "She is nicknamed the '____ in the boat'" : "GOAT"}
