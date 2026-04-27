09d616aa0076d0304cf6a73b2db06770381ed2e6
partnern <partnern@stu.otc.school.nz>
2026-04-28 10:50:19 +1200

    Asking user multi choice question and seeing if answer is valid. Not tested.

--- NevePartner 92004.py
+++ NevePartner 92004.py
@@ -8,13 +8,27 @@ print("The first five questions will be multi choice, and the last five will be 
 #Creating the first five multi choice questions
 print('For the multi choice questions, please input the number corresponding to the answer you think is correct.')
 multiQandA = {"How many Olympic Games has she competed in?\n1. 1\n2. 2\n3. 3\n4. 4\n" : 4,
-                  "Which of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n" : 2,
-                  "What sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoeing\n3. Kayaking\n4. Whitewater rafting\n" : 2,
-                  "When did she obtain the record for world's fastest K1 200m?\n1. 2010\n2.2012\n3. 2014\n4. 2016\n" : 3,
-                  "How many Olympic bronze medals has she won?\n1. 1\n2. 2\n3. 3\n4. 4\n" : 1}
+              "Which of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris\n" : 2,
+              "What sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoeing\n3. Kayaking\n4. Whitewater rafting\n" : 2,
+              "When did she obtain the record for world's fastest K1 200m?\n1. 2010\n2.2012\n3. 2014\n4. 2016\n" : 3,
+              "How many Olympic bronze medals has she won?\n1. 1\n2. 2\n3. 3\n4. 4\n" : 1}
 #Asking the user for the multi choice answers
 for question, answer in multiQandA.items():
-    print(question)
+    answer = input(question)
+    #Seeing if answer is valid
+    valid = False
+    while valid == False:
+        try:
+            int(answer)
+            if answer < 0:
+                print('Please enter a number that corresponds to an answer')
+            elif answer > 4:
+                print('Please enter a number that corresponds to an answer')
+            else:
+                valid = True
+                print('answer is valid')
+        except:
+           print('Please enter an integer')
 
 # #Creating a dictionary with user input questions and answers
 questions_answers = {"What year was Lisa Carrington born?" : 1989,
