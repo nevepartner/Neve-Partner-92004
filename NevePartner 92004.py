a490349e8950eb800fcb15883e5ed91adfe003e5
partnern <partnern@stu.otc.school.nz>
2026-05-06 15:00:31 +1200

    Decided to make all questions multi choice. Also added a user friendly message at the end to tell them how they did.

--- NevePartner 92004.py
+++ NevePartner 92004.py
@@ -3,78 +3,68 @@
 
 #Introducing the audience to the quiz
 print("This is a quiz on Lisa Carrington, one of New Zealand's greatest sporting stars.")
-print("The first five questions will be multi choice, and the last five will be user input.\n")
+print("The questions are all multi choice")
 
 correct_answers = 0
 total_questions = 10
+minimum_value = 1
+max_value = 4
 
 #Creating the first five multi choice questions
-print('For the multi choice questions, please input the number corresponding to the answer you think is correct.')
-multiQandA = {"\nHow many Olympic Games has she competed in?\n1. 1\n2. 2\n3. 3\n4. 4" : 4,
-              "\nWhich of these places did Lisa Carrington not win an Olympic medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris" : 2,
-              "\nWhat sport does Lisa Carrington compete in?\n1. Rowing\n2. Canoeing\n3. Kayaking\n4. Whitewater rafting" : 2,
-              "\nWhen did she obtain the record for world's fastest K1 200m?\n1. 2010\n2.2012\n3. 2014\n4. 2016" : 3,
-              "\nHow many Olympic bronze medals has she won?\n1. 1\n2. 2\n3. 3\n4. 4" : 1}
+print("Please input the number corresponding to the answer you think is correct.")
+questions_and_answers = {"\nHow many Olympic Games has she competed in?\n1. 1\n2. 2\n3. 3\n"
+              "4. 4" : 4,
+              "\nWhich of these places did Lisa Carrington not win an Olympic" 
+              "medal at?\n1. Tokyo\n2. Sydney\n3. Rio\n4. Paris" : 2,
+              "\nWhat sport does Lisa Carrington compete in?\n1. Rowing\n2. "
+              "Canoeing\n3. Kayaking\n4. Whitewater rafting" : 2,
+              "\nWhen did she obtain the record for world's fastest K1 200m?\n"
+              "1. 2010\n2. 2012\n3. 2014\n4. 2016" : 3,
+              "\nHow many Olympic bronze medals has she won?\n1. 1\n2. 2\n3. 3\n"
+              "4. 4" : 1,
+              "\nWhat year was Lisa Carrington born?\n1. 1983\n2. 1985\n"
+              "3. 1987\n4. 1989" : 4,
+              "\nHow many Olympic medals has she won?\n1. 6\n2. 7\n3. 8\n4. 9"
+              : 4,
+              "\nWhat city was she born in?\n1. Aukland\n2. Tauranga\n3. Wellington\n"
+              "4. Christchurch" : 2,
+              "\nHow many World Championship gold medals has she won?\n1. 5\n"
+              "2. 10\n3. 15\n4. 20" : 3,
+              "\n When did she win her first Olympic gold medal?\n1. 2010\n2. 2012\n"
+              "3. 2014\n4. 2016" : 2}
 
 #Asking the user for the multi choice answers
-for question, answer in multiQandA.items():
+for question, answer in questions_and_answers.items():
     print(question)
     #Seeing if answer is valid
     valid = False
     while valid == False:
-        multi_answer = input('Your answer: ')
+        user_answer = input('Your answer: ')
         try:
-            multi_answer = int(multi_answer)
-            if multi_answer < 1:
+            user_answer = int(user_answer)
+            if user_answer < minimum_value:
                 print('Please enter a number that corresponds to an answer')
-            elif multi_answer > 4:
+            elif user_answer > max_value:
                 print('Please enter a number that corresponds to an answer')
             else:
                 valid = True
         except:
            print('Please enter an integer')
     #Seeing if the answer is correct
-    if multi_answer == answer:
+    if user_answer == answer:
         print('Correct!')
         correct_answers = correct_answers + 1
     else:
         print(f'Incorrect. The correct answer was {answer}.')
 
-# #Creating a dictionary with user input questions and answers
-user_inputQandA = {"\nWhat year was Lisa Carrington born?" : 1989,
-                   "\nHow many Olympic medals has she won?" : 9,
-                   "\nWhat city was she born in?" : "Tauranga",
-                   "\nHow many World Championship gold medals has she won?" : 15,
-                   "\nShe is nicknamed the '____ in the boat'" : "GOAT"}
+#Calculating users final score. I multiplied the score by 10 to get a percentage.
+percentage_correct = correct_answers / total_questions
+percentage_correct = percentage_correct * 100
 
-#Asking the user for the user for the user input answers
-for question, answer in user_inputQandA.items():
-    print(question)
-    #Seeing if the correct answer is an integer or a string so I can better test
-    #for invalid user input
-    try:
-        answer = int(answer)
-        type_of_data = 'int'
-    except:
-        type_of_data = 'str'
-    #Testing if the user input is valid
-    valid = False
-    user_answer = input('Your answer: ')
-    while valid == False:
-        if type_of_data == 'int':
-            try:
-                user_answer = int(user_answer)
-                valid = True
-            except:
-                print('Please enter a number')
-        if type_of_data == 'str':
-            try:
-                user_answer = str(user_answer)
-                valid = True
-            except:
-                print('Please enter a word')
-    if user_answer == answer:
-        print('Correct')
-        correct_answers = correct_answers + 1
-    else:
-        print(f'Incorrect. The correct answer was {answer}')
+#Printing a message to show user how they did
+if correct_answers < 5:
+    print(f"\nWell done! You got {correct_answers} correct. That's {percentage_correct}%")
+elif correct_answers < 8:
+    print(f"\nGreat job! You got {correct_answers} correct. That's {percentage_correct}%")
+else:
+    print(f"\nExcellent work! You got {correct_answers} correct. That's {percentage_correct}%")
